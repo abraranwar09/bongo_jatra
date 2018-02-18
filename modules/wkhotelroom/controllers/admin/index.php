5cbb538d5933331e89c89e9828c7370ecf312123
Abrar <redhouse@Reds-MacBook-Pro.local>
2018-02-18 14:55:39 +0600

    Initial commit

---
+++ modules/wkhotelroom/controllers/admin/index.php
@@ -0,0 +1,34 @@
+<?php
+
+
+
+
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>
+		
+
+
+		Rebel Force 
+	</title>
+</head>
+<body>
+
+
+<div class="navigation"> 
+	
+<img class="brand" src="bhhal/ball/img.jpg"
+</div>
+<h1> HELLO WORLD </h1> 
+
+ 
+
+
+
+
+</body>
+</html>
